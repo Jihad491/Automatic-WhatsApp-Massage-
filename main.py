bbf3b896f6720a99484ab00c05505f97c9f8b90d
Jihad491 <mdjihadahmed491@gmail.com>
2025-10-23 23:59:13 +0600

    Number Added

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import pywhatkit
+import tkinter as tk
+root = tk.Tk()
+root.title("WhatsApp Massege")
+root.geometry("600x600")
+lab1 =tk.Label(root,text="Enter Your Number",font=("Arial",15))
+lab1.pack(pady=10)
+number = tk.Entry(root);
+number.pack(pady=10)
+submit = tk.Button(root,text=("Submit"))
+submit.pack(pady=10)
+root.mainloop()
