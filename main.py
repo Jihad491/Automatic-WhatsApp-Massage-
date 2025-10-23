4375655623f43b0ba59849aa193b2e747e365ddc
Jihad491 <mdjihadahmed491@gmail.com>
2025-10-24 00:06:40 +0600

    button added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import tkinter as tk
 root = tk.Tk()
 root.title("WhatsApp Massege")
 root.geometry("600x600")
+def massege():
+    pywhatkit.sendwhatmsg(str(number.get()),"Hi",12,4)
 lab1 =tk.Label(root,text="Enter Your Number",font=("Arial",15))
 lab1.pack(pady=10)
 number = tk.Entry(root);
