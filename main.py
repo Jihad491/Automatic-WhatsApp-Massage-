d53ae343aa281adcb3c7419b6afc46966a27f65d
Jihad491 <mdjihadahmed491@gmail.com>
2025-10-24 10:34:00 +0600

    Error fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ root = tk.Tk()
 root.title("WhatsApp Massege")
 root.geometry("600x600")
 def massege():
-    pywhatkit.sendwhatmsg(str(number.get()),str(text.get()),int(hr.get()),int(miniute.get()))
+    pywhatkit.sendwhatmsg(str(number.get()),str(text.get("1.0", "end-1c")),int(hr.get()),int(miniute.get()))
 lab1 =tk.Label(root,text="Enter Your Number",font=("Arial",15))
 lab1.pack(pady=10)
 number = tk.Entry(root);
@@ -15,10 +15,14 @@ text = tk.Text(root,height=10,width=30)
 text.pack(pady=10)
 lab3 =tk.Label(root,text=("Enter sending time"),font=("Arial",15))
 lab3.pack(pady=10)
+
+#TIme
 hr = tk.Entry(root)
 hr.pack(pady=10)
 miniute = tk.Entry(root)
 miniute.pack(pady=10)
+
+#Submission
 submit = tk.Button(root,text=("Submit"),command=massege)
 submit.pack(pady=10)
 root.mainloop()
